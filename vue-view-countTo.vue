236f664f361217b919b3e3a7bdc692ba9bbae3db
zhouzhou <630551760@qq.com>
2019-05-25 11:23:01 +0800

    vue-countTo

---
+++ vue-view-countTo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <CountTo ref="countTo" @input='handleinput' :end-val='endVal'></CountTo>
+    <button @click="getInnerNum">点我获取</button>
+  </div>
+</template>
+
+
+<script>
+import CountTo from './vue-countTo.vue'
+import { parse } from 'path';
+export default {
+  data(){
+    return {
+      data:0,
+      endVal:100
+    }
+  },
+  components:{
+    CountTo
+  },
+  methods:{
+    handleinput(val){
+      console.log(val)
+      if(!val.length){
+        this.endVal = 0      
+      }else{
+        this.endVal = val && parseInt(val)
+      }   
+    },
+    getInnerNum(){
+      console.log(this.$refs.countTo.getCount())
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
